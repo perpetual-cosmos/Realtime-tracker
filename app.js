89d5d8dee9a571acf944dbadab079bf76bd4cab2
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-10-07 18:16:46 +0530

    doen

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const express =require("express");
 const app=express();
+const http=require("http");
+const path = require("path");
+const socketio=require("socket.io");
 
+//creating server
+const server=http.createServer(app)
+
+//creating socket
+const io=socketio(server);
+
+
+//ejs setup
+app.set("view engin","ejs");
+app.set(express.static(path.join(__dirname,"public")));
+
+
+//listening to port 3000
+server.listen(3000,()=>{
+    console.log('listening to port 3000');
+})
