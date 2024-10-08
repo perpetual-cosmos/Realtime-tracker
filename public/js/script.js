156f9ce282044ac04fb6804ecf2ef433c73e3f3c
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-10-08 19:45:35 +0530

    near to finish

--- public/js/script.js
+++ public/js/script.js
@@ -15,4 +15,20 @@ if(navigator.geolocation){
     
 }
 
-L.map("map")
+const map=L.map("map").setView([0,0],16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    atrribution:"openStreetMap"
+}).addTo(map)
+
+const markers={};
+
+socket.on("recieve-location",(data)=>{
+    const {id,latitude,longitude}=data;
+    map.setView([latitude,longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude,longitude]);
+    }
+    else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map);
+    }
+})
